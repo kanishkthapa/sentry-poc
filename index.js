6ecf4583e00d9b676a0db4bb477e33f9e7bef44c
kanishkthapa <kanishk@betalectic.com>
2023-12-13 17:08:34 +0530

    sentry catch exception

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import * as Sentry from "@sentry/node";
+const Sentry = require("@sentry/node");
 
 Sentry.init({
   dsn: "https://a8826fdcd2eff309188c1e692d2c8059@o4506387687931904.ingest.sentry.io/4506387950993408",
@@ -7,3 +7,19 @@ Sentry.init({
   // for finer control
   tracesSampleRate: 1.0,
 });
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException(e);
+    throw e;
+  } finally {
+    transaction.finish();
+  }
+}, 99);
