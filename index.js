9f84f57abea01049ef8912d3ed1a27244c746a9b
kanishkthapa <kanishk@betalectic.com>
2023-12-14 10:55:54 +0530

    added sentry/profiling-node

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const Sentry = require("@sentry/node");
+const { ProfilingIntegration } = require("@sentry/profiling-node");
 
 Sentry.init({
   dsn: "https://a8826fdcd2eff309188c1e692d2c8059@o4506387687931904.ingest.sentry.io/4506387950993408",
 
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
+  integrations: [
+    // Add our Profiling integration
+    new ProfilingIntegration(),
+  ],
   tracesSampleRate: 1.0,
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
 });
 
 const transaction = Sentry.startTransaction({
